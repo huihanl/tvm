e724b6c99420d711c5a9739bdba7fd580d731a21
Huihan Liu <liuhh@berkeley.edu>
2019-06-04 16:56:58 -0700

    add requantization for GEMM with packed B; add test function in test_fbgemm
    fixed function name
    
    more debug
    
    debug
    
    edit column offset
    
    debug pointer for column offsets
    
    debug pointer for column offsets again
    
    change arg type for column offsets
    
    debug
    
    fixed function name
    
    more debug
    
    debug
    
    edit column offset
    
    debug pointer for column offsets
    
    debug pointer for column offsets again
    
    change arg type for column offsets
    
    debug
    
    more testing
    
    implement the prepacked matrix multiplication with requantization
    
    add to test_fbgemm_packedW_autotuned.py
    
    test if column offset within fc_int8 works
    
    revert to previous version
    
    print col offset
    
    test print content col offset
    
    finish requant
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    clean up
    
    debug
    
    add test cases
    
    finish requantization for prepacked weights
    
    add requantization for GEMM with prepacked weight

--- src/contrib/fbgemm/fully_connected_int8.cc
+++ src/contrib/fbgemm/fully_connected_int8.cc
@@ -52,7 +52,12 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.print_col_offsets")
       void* co = args[0];
       std::vector<std::int32_t>* coffsts =
           reinterpret_cast<std::vector<std::int32_t>*>(co);
-      std::cout << "size of col offsets" << coffsts->size() << std::endl;
+      std::cout << "size of col offsets" << coffsts->size() << " " << std::endl;
+
+      for(int i=0; i<coffsts->size(); i++) {
+        std::cout << coffsts->at(i) << ' ';
+      }
+
     });
 
 TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.pack_matrixB_int8")
@@ -75,7 +80,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.pack_matrixB_int8")
         params.NR = args[cntr + 4];
         params.NR_MIN = args[cntr + 5];
         params.ROW_INTERLEAVE = args[cntr + 6];
-  
+
         auto packB = new PackBMatrix<std::int8_t, std::int32_t>(
             matrix_op_t::NoTranspose, k, n,
             reinterpret_cast<const std::int8_t*>(W->data), n, nullptr, 1,
@@ -96,9 +101,10 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.compute_col_offsets_int8")
       int threads = args[1];
       std::int32_t w_zero_point = args[2];
 
+      //std::cout << W->shape << ' ';
       CHECK_EQ(W->ndim, 2);
-      int n = W->shape[0];
-      int k = W->shape[1];
+      int k = W->shape[0];
+      int n = W->shape[1];
 
       std::vector<TensorQuantizationParams> temp_qparams;
       temp_qparams.push_back(TensorQuantizationParams{1.0, w_zero_point});
@@ -119,7 +125,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt")
       void* weight = reinterpret_cast<void*>(static_cast<uint64_t>(wt));
       packbmatrix* packB =
           reinterpret_cast<PackBMatrix<std::int8_t, std::int32_t>*>(weight);
-
+      packB->printPackedMatrix("packB");
       DLTensor* B = args[2];  // N quantized int8 bias
       DLTensor* Y = args[3];
       int threads = args[9];
@@ -167,9 +173,9 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt")
 
       fbgemmPacked(packA, *packB, reinterpret_cast<std::int32_t*>(Y->data),
                   reinterpret_cast<std::int32_t*>(Y->data), n, memcopyObj, 0,
-                   threads, &params); 
+                   threads, &params);
 
-	} /*else{
+	    } /*else{
 
       PackAWithRowOffset<std::uint8_t> packA(
           matrix_op_t::NoTranspose, m, k,
@@ -180,9 +186,108 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt")
 
       fbgemmPacked(packA, *packB, reinterpret_cast<std::int32_t*>(Y->data),
                   reinterpret_cast<std::int32_t*>(Y->data), n, memcopyObj, 0,
-                   threads); 
-     }*/ 
+                   threads);
+     }*/
 });
+
+TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt_with_requant")
+    .set_body([](TVMArgs args, TVMRetValue* ret) {
+      DLTensor* X = args[0];  // M*K quantized int8 input
+      std::uint64_t wt = args[1];
+      void* weight = reinterpret_cast<void*>(static_cast<uint64_t>(wt));
+      packbmatrix* packB =
+          reinterpret_cast<PackBMatrix<std::int8_t, std::int32_t>*>(weight);
+
+      packB->printPackedMatrix("packB");
+
+      DLTensor* B = args[2];  // N quantized int8 bias
+      // ignore the axis and axis_w now for testing purpose
+      DLTensor* Y = args[3];
+      int threads = args[9];
+      //DLTensor* W = args[10];
+
+      //CHECK_EQ(X->ndim, 2);
+      //CHECK_EQ(W->ndim, 2);
+      //CHECK_EQ(B->ndim, 1);
+      //CHECK_EQ(X->shape[1], W->shape[1]);
+      //CHECK_EQ(W->shape[0], B->shape[0]);
+
+      float ReQuant_multiplier = (double)args[7];
+      std::int32_t x_zero_point = args[4];
+      std::int32_t w_zero_point = args[5];
+      std::int32_t y_zero_point = args[6];
+
+      int m = X->shape[0];
+      int n = Y->shape[1];
+      int k = X->shape[1];
+
+      BlockingFactors params;
+
+      if(args.size() > 10) {
+        int cntr = 10;
+        params.MCB = args[cntr];
+        params.NCB = args[cntr + 1];
+        params.KCB = args[cntr + 2];
+        params.MR = args[cntr + 3];
+        params.NR = args[cntr + 4];
+        params.NR_MIN = args[cntr + 5];
+        params.ROW_INTERLEAVE = args[cntr + 6];
+      }
+
+      std::vector<std::int32_t> row_offsets_(
+          PackAWithRowOffset<uint8_t>::rowOffsetBufferSize());
+      std::vector<std::int32_t> Y_int32_(n * m);
+
+      std::uint64_t co_addr = args[8];
+      void* co = reinterpret_cast<void*>(static_cast<uint64_t>(co_addr));
+
+      std::vector<std::int32_t>* column_offsets_ =
+          reinterpret_cast<std::vector<std::int32_t>*>(co);
+
+      for(int i=0; i<column_offsets_->size(); i++) {
+            std::cout << column_offsets_->at(i) << ' ';
+      }
+
+      std::vector<TensorQuantizationParams> temp_qparams;
+      temp_qparams.push_back(TensorQuantizationParams{1.0, w_zero_point});
+
+      if(args.size() > 10){
+
+        PackAWithRowOffset<std::uint8_t> packA(
+            matrix_op_t::NoTranspose, m, k,
+            reinterpret_cast<const std::uint8_t*>(X->data), k, nullptr, 1,
+            row_offsets_.data(), &params);
+
+        DoNothing<> doNothingObj{};
+        ReQuantizeOutput<false> outputProcObj(
+            doNothingObj, &ReQuant_multiplier, y_zero_point, x_zero_point,
+            &w_zero_point, packA.getRowOffsetBuffer(), (*column_offsets_).data(),
+            reinterpret_cast<const std::int32_t*>(B->data), n);
+
+        fbgemmPacked(packA, *packB, reinterpret_cast<std::uint8_t*>(Y->data),
+                     Y_int32_.data(), n, outputProcObj, 0, threads,
+                     &params);  // num_threads
+
+      }  else {
+
+        PackAWithRowOffset<std::uint8_t> packA(
+            matrix_op_t::NoTranspose, m, k,
+            reinterpret_cast<const std::uint8_t*>(X->data), k, nullptr, 1,
+            row_offsets_.data());
+
+        DoNothing<> doNothingObj{};
+        ReQuantizeOutput<false> outputProcObj(
+            doNothingObj, &ReQuant_multiplier, y_zero_point, x_zero_point,
+            &w_zero_point, packA.getRowOffsetBuffer(), (*column_offsets_).data(),
+            reinterpret_cast<const std::int32_t*>(B->data), n);
+
+        fbgemmPacked(packA, *packB, reinterpret_cast<std::uint8_t*>(Y->data),
+                     Y_int32_.data(), n, outputProcObj, 0,
+                     threads);  // num_threads
+      }
+});
+
+
 
 bool isValid(BlockingFactors *param)
 {
@@ -226,7 +331,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt_for_tuning")
       //void* col_offsts = reinterpret_cast<void*>(static_cast<uint64_t>(co));
       //std::vector<std::int32_t>* column_offsets_ =
       //    reinterpret_cast<std::vector<std::int32_t>*>(col_offsts);
-    
+
      BlockingFactors params;
      if(args.size() > 10){
         int cntr = 10;
@@ -239,7 +344,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt_for_tuning")
         params.ROW_INTERLEAVE = args[cntr + 6];
 
         assert (isValid(&params) == true  && "incorrect configuration");
-        
+
 
         static PackBMatrix<std::int8_t, std::int32_t> packB_ (
             matrix_op_t::NoTranspose, k, n,
@@ -248,7 +353,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt_for_tuning")
 
       PackAMatrix<std::uint8_t> packA(
           matrix_op_t::NoTranspose, m, k,
-          reinterpret_cast<const std::uint8_t*>(X->data), k, nullptr, 1, 
+          reinterpret_cast<const std::uint8_t*>(X->data), k, nullptr, 1,
           &params);
 
       DoNothing<std::int32_t, std::int32_t> doNothing32BitObj;
@@ -256,7 +361,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.gemmint8acc32packedwt_for_tuning")
 
       fbgemmPacked(packA, packB_, reinterpret_cast<std::int32_t*>(Y->data),
                   reinterpret_cast<std::int32_t*>(Y->data), n, memcopyObj, 0,
-                   threads, &params); 
+                   threads, &params);
 	}
     });
 
@@ -269,11 +374,11 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.fully_connected_int8")
       DLTensor* Y = args[3];
       int threads = args[8];
 
-      CHECK_EQ(X->ndim, 2);
-      CHECK_EQ(W->ndim, 2);
-      CHECK_EQ(B->ndim, 1);
-      CHECK_EQ(X->shape[1], W->shape[1]);
-      CHECK_EQ(W->shape[0], B->shape[0]);
+      //CHECK_EQ(X->ndim, 2);
+      //CHECK_EQ(W->ndim, 2);
+      //CHECK_EQ(B->ndim, 1);
+      //CHECK_EQ(X->shape[1], W->shape[1]);
+      //CHECK_EQ(W->shape[0], B->shape[0]);
 
       float ReQuant_multiplier = (double)args[7];
       std::int32_t x_zero_point = args[4];
@@ -281,7 +386,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.fully_connected_int8")
       std::int32_t y_zero_point = args[6];
 
       int m = X->shape[0];
-      int n = W->shape[0];
+      int n = Y->shape[1];
       int k = X->shape[1];
 
       BlockingFactors params;
@@ -306,7 +411,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.fully_connected_int8")
 
       if (args.size() > 9) {
         PackBMatrix<std::int8_t, std::int32_t> packB(
-            matrix_op_t::Transpose, k, n,
+            matrix_op_t::NoTranspose, k, n,
             reinterpret_cast<const std::int8_t*>(W->data), k, nullptr, 1,
             &params);
 
@@ -331,7 +436,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.fully_connected_int8")
 
       } else {
         PackBMatrix<std::int8_t, std::int32_t> packB(
-            matrix_op_t::Transpose, k, n,
+            matrix_op_t::NoTranspose, k, n,
             reinterpret_cast<const std::int8_t*>(W->data), k, nullptr, 1);
 
         PackAWithRowOffset<std::uint8_t> packA(
