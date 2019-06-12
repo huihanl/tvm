37c5d35b81b860b17a38740b3a8fea892d090f67
Yinghai Lu <liuhh@berkeley.edu>
2019-06-12 15:04:24 -0700

    Revert "debug"
    This reverts commit 72d4531df7a649dc05540082f0098ee0c58fe6d8.

--- src/contrib/fbgemm/fully_connected_int8.cc
+++ src/contrib/fbgemm/fully_connected_int8.cc
@@ -612,7 +612,7 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.conv_int8")
           OC_per_G,
           reinterpret_cast<std::int8_t*>(B->data) + g * KDimPerGroup * OC_per_G,
           //Bint8_zero_point.data(),
-          Bint8_zero_point->data(),
+          Bint8_zero_point,
           col_offsets.data() + g * OC_per_G,
           conv_p.OC);
     }
@@ -627,11 +627,11 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.conv_int8")
     ReQuantizeOutput<false, QuantizationGranularity::TENSOR> outputProcObj(
         doNothingObj,
         //C_multiplier.data(),
-        C_multiplier->data(),
+        C_multiplier,
         C_zero_point,
         Aint8_zero_point,
         //Bint8_zero_point.data(),
-        Bint8_zero_point->data(),
+        Bint8_zero_point,
         nullptr, // row offsets
         col_offsets.data(),
         nullptr, // bias
@@ -648,7 +648,6 @@ TVM_REGISTER_GLOBAL("tvm.contrib.fbgemm.conv_int8")
         0,
         1);
     });
-
 
 }  // namespace contrib
 }  // namespace tvm
