95e9456361411656e469e4d665f912645e050680
huihanl <liuhh@berkeley.edu>
2019-07-08 13:52:11 -0700

    finish

--- tests/python/contrib/test_fbgemm_packedW_autotuned.py
+++ tests/python/contrib/test_fbgemm_packedW_autotuned.py
@@ -272,14 +272,21 @@ def im2col_ref(MB, IC, OC, IN_DIM, OUT_DIM, G, K, stride, pad, A, A_zero_point, 
 
 
 
-
-def requantize_u8acc32_ref(M, N, ld, inp, C_multiplier, C_zero_point,
-                           A_zero_point, B_zero_point, row_offsets, col_offsets,
-                           ncols_per_quant_group, NDim, NDim_OC, G):
+def requantize_u8acc32_ref(M1, K1, ld1, Aint81, length1, KDimPerGroup1, G,
+			    M, N, ld, inp, C_multiplier, C_zero_point,
+                           A_zero_point, B_zero_point, col_offsets,
+                           ncols_per_quant_group, NDim, NDim_OC):
     out = [0 for i in range(len(inp))]
-
+    row_offsets = [0 for i in range(length1)]
     for g in range(G):
-        for i in range(M):
+        for i in range(M1):
+            sum = 0
+            for k in range(K1):
+                sum += Aint81[KDimPerGroup1 * g + i * ld1 + k]
+            row_offsets[i] = sum
+
+
+	for i in range(M):
             for j in range(N):
                 raw = inp[NDim * g + i * ld + j]
                 if A_zero_point:
@@ -288,20 +295,7 @@ def requantize_u8acc32_ref(M, N, ld, inp, C_multiplier, C_zero_point,
                 result = raw * C_multiplier[NDim_OC * g + j / ncols_per_quant_group]
                 rounded = round(result) + C_zero_point
                 out[NDim * g + i * ld + j] = max(0, min(255, rounded))
-
     return out
-
-
-def row_offsets_u8acc32_ref(M, K, ld, Aint8, length, KDimPerGroup, G):
-    row_offsets = [0 for i in range(length)]
-    for g in range(G):
-        for i in range(M):
-            sum = 0
-            for k in range(K):
-                sum += Aint8[KDimPerGroup * g + i * ld + k]
-            row_offsets[i] = sum
-    return row_offsets
-
 
 def col_offsets_with_zero_pt_s8acc32_ref(K, N, ld, OC, Bint8, B_zero_point, ncols_per_quant_group, G, col_lead, w_lead):
     col_offsets = [0 for i in range(OC)]
@@ -319,6 +313,7 @@ def reference_solution(A, A_zero_point, W, MB, IC, OC, IN_DIM, OUT_DIM, G, K, st
     Cint32_ref = conv_ref(MB, IC, OC, IN_DIM, OUT_DIM, G, K, stride, pad,
                  A, A_zero_point, W)
 
+
     im_in_dim = IN_DIM[0] * IN_DIM[1]
     kernel_dim = K[0] * K[1]
     im_out_dim = OUT_DIM[0] * OUT_DIM[1]
@@ -332,23 +327,16 @@ def reference_solution(A, A_zero_point, W, MB, IC, OC, IN_DIM, OUT_DIM, G, K, st
 
     length_im2col = MDim * KDim
     A_im2col = im2col_ref(MB, IC, OC, IN_DIM, OUT_DIM, G, K, stride, pad, A, A_zero_point, length_im2col)
-    print("A_im2col: ")
-    print(A_im2col)
     col_lead = OC_per_G
     w_lead = KDimPerGroup * OC_per_G
     col_offsets = col_offsets_with_zero_pt_s8acc32_ref(KDimPerGroup, OC_per_G, OC_per_G, OC,
                                                        W, B_zero_point, OC, G, col_lead, w_lead)
-    print("col_offsets: ")
-    print(col_offsets)
-
-    row_offsets = row_offsets_u8acc32_ref(MDim, KDimPerGroup, KDim, A_im2col, MDim, KDimPerGroup, G)
-    print("row_offsets: ")
-    print(row_offsets)
 
     NDim_OC = NDim / OC
-    output = requantize_u8acc32_ref(MDim, NDim, G * NDim, Cint32_ref, C_multiplier, C_zero_point,
-                                    A_zero_point, B_zero_point, row_offsets, col_offsets,
-                                    OC, NDim, NDim_OC, G)
+    output = requantize_u8acc32_ref(MDim, KDimPerGroup, KDim, A_im2col, MDim, KDimPerGroup, G,
+				    MDim, NDim, G * NDim, Cint32_ref, C_multiplier, C_zero_point,
+                                    A_zero_point, B_zero_point, col_offsets,
+                                    OC, NDim, NDim_OC)
 
     return output
 
@@ -501,7 +489,6 @@ if __name__ == "__main__":
     valid = f.readlines()
     for i in range(len(valid) / 3):
 	config = configs[i]
-	print(i)
         xref = valid[i * 3]
         wref = valid[i * 3 + 1]
 	yref = valid[i * 3 + 2]
